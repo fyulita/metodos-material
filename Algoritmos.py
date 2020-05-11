7364e94e8312064ef5b50f300b0766827624a529
fyulita <federico_yulita@hotmail.com>
2020-05-11 14:19:19 -0300

    Complete los algoritmos.

--- Algoritmos.py
+++ Algoritmos.py
@@ -1,49 +1,144 @@
 import numpy as np
 
 
+def is_matrix(A):
+    return not (len(A.shape) != 2 or A.shape[0] != A.shape[1])
+
+
+def is_inversible(A):
+    assert (is_matrix(A))
+    return not np.isclose(np.abs(np.linalg.det(A)), 0)
+
+
+def are_LI(v, w):
+    assert (len(v) == len(w))
+    n = len(v)
+    ans = False
+
+    f = 0
+    for i in range(n):
+        if (v[i] == 0 and w[i] != 0) or (w[i] == 0 and v[i] != 0):
+            ans = True
+        elif (v[i] != 0 and w[i] != 0) and f == 0:
+            f = v[i] / w[i]
+        elif (v[i] != 0 and w[i] != 0) and (v[i] / w[i] != f):
+            ans = True
+
+    return ans
+
+
+def has_one_solution(A):
+    assert (is_matrix(A))
+    n = len(A)
+    ans = True
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            if not are_LI(A[i], A[j]):
+                ans = False
+
+    return ans
+
+
+def is_symmetric(A):
+    assert (is_matrix(A))
+    return np.allclose(A, A.T)
+
+
+def is_diagonal(A):
+    assert (is_matrix(A))
+    n = len(A)
+    ans = True
+    for i in range(n):
+        for j in range(n):
+            if i != j and not np.isclose(A[i, j], 0):
+                ans = False
+
+    return ans
+
+
+def is_orthogonal(A):
+    assert (is_inversible(A))
+    return np.allclose(A.T, np.linalg.inv(A))
+
+
+def is_diagonally_dominant(A):
+    assert (is_matrix(A))
+    n = len(A)
+    ans = True
+    for i in range(n):
+        sum = 0
+        for j in range(n):
+            if i != j:
+                sum = sum + np.abs(A[i, j])
+        if np.abs(A[i, i]) < sum:
+            ans = False
+
+    return ans
+
+
+def is_diagonally_strictly_dominant(A):
+    assert (is_matrix(A))
+    n = len(A)
+    ans = True
+    for i in range(n):
+        sum = 0
+        for j in range(n):
+            if i != j:
+                sum = sum + np.abs(A[i, j])
+        if np.abs(A[i, i]) <= sum:
+            ans = False
+
+    return ans
+
+
+def has_LU(A):
+    assert (is_matrix(A))
+    n = len(A)
+    ans = True
+    for i in range(1, n):
+        B = A[:i, :i]
+        ans = is_inversible(B)
+
+    return ans
+
+
 def gauss_no_perm(A):
+    assert (is_matrix(A))
     n = len(A)
     ans = A.copy()
 
     for i in range(n - 1):
         for j in range(i + 1, n):
-            f = ans[j][i] / ans[i][i]
+            f = ans[j, i] / ans[i, i]
             for k in range(i, n):
-                ans[j][k] = ans[j][k] - f * ans[i][k]
+                ans[j, k] = ans[j, k] - f * ans[i, k]
 
     return ans
 
 
 def gauss(A):
+    assert (is_matrix(A))
+
     def swap(M, i, j):
         temp = M[i].copy()
         M[i] = M[j]
         M[j] = temp
-
-    def column_slice(M, col, begin, end):
-        ans = []
-        for i in range(len(M)):
-            if i < begin or i >= end:
-                ans.append(-1)
-            else:
-                ans.append(np.abs(M[i][col]))
-
-        return ans
 
     n = len(A)
     ans = A.copy()
     row = 0
     column = 0
     while row < n and column < n:
-        i_max = np.argmax(column_slice(ans, column, row, n))
-        if ans[i_max][column] == 0:
+        i_max = np.argmax(ans[row:, column]) + row
+        if ans[i_max, column] == 0:
             column += 1
         else:
             swap(ans, row, i_max)
             for i in range(row + 1, n):
-                f = ans[i][column] / ans[row][column]
-                for j in range(column + 1, n):
-                    ans[i][j] = ans[i][j] - f * ans[row][j]
+                f = ans[i, column] / ans[row, column]
+                for j in range(column, n):
+                    ans[i, j] = ans[i, j] - f * ans[row, j]
             row += 1
             column += 1
 
@@ -51,20 +146,12 @@ def gauss(A):
 
 
 def solve(A, b):
+    assert (has_one_solution(A))
+
     def swap(M, i, j):
         temp = M[i].copy()
         M[i] = M[j]
         M[j] = temp
-
-    def column_slice(M, col, begin, end):
-        ans = []
-        for i in range(len(M)):
-            if i < begin or i >= end:
-                ans.append(-1)
-            else:
-                ans.append(np.abs(M[i][col]))
-
-        return ans
 
     n = len(A)
     A_work = A.copy()
@@ -72,16 +159,16 @@ def solve(A, b):
     row = 0
     column = 0
     while row < n and column < n:
-        i_max = np.argmax(column_slice(A_work, column, row, n))
-        if A_work[i_max][column] == 0:
+        i_max = np.argmax(A_work[row:, column]) + row
+        if A_work[i_max, column] == 0:
             column += 1
         else:
             swap(A_work, row, i_max)
             swap(ans, row, i_max)
             for i in range(row + 1, n):
-                f = A_work[i][column] / A_work[row][column]
+                f = A_work[i, column] / A_work[row, column]
                 for j in range(column, n):
-                    A_work[i][j] = A_work[i][j] - f * A_work[row][j]
+                    A_work[i, j] = A_work[i, j] - f * A_work[row, j]
                 ans[i] = ans[i] - f * ans[row]
             row += 1
             column += 1
@@ -90,14 +177,15 @@ def solve(A, b):
     while k >= 0:
         sum = ans[k]
         for l in range(k + 1, n):
-            sum = sum - A_work[k][l] * ans[l]
-        ans[k] = sum / A_work[k][k]
+            sum = sum - A_work[k, l] * ans[l]
+        ans[k] = sum / A_work[k, k]
         k -= 1
 
     return ans
 
 
 def LU(A):
+    assert (has_LU(A))
     n = len(A)
     L = np.identity(n)
     U = A.copy()
@@ -105,52 +193,31 @@ def LU(A):
     for i in range(n - 1):
         temp = np.identity(n)
         for j in range(i + 1, n):
-            f = U[j][i] / U[i][i]
+            f = U[j, i] / U[i, i]
             for k in range(i, n):
-                U[j][k] = U[j][k] - f * U[i][k]
-            temp[j][i] = -f
-        L = np.dot(temp, L)
+                U[j, k] = U[j, k] - f * U[i, k]
+            temp[j, i] = -f
+        L = temp @ L
 
     L = np.linalg.inv(L)
 
-    return U, L
+    return L, U
+
+
+def LDL(A):
+    assert (has_LU(A))
+    assert (is_symmetric(A))
+
+    L, U = LU(A)
+    D = np.zeros(U.shape)
+    for i in range(len(D)):
+        D[i, i] = U[i, i]
+
+    return L, D
 
 
 def cholesky(A):
-    n = len(A)
-    L = np.zeros([n, n])
-    L[0][0] = np.sqrt(A[0][0])
-    for i in range(1, n):
-        L[i][0] = A[i][0] / L[0][0]
-    for j in range(1, n):
-        sum1 = 0
-        for k in range(j):
-            sum1 = sum1 + L[j][k] ** 2
-        L[j][j] = np.sqrt(A[j][j] - sum1)
-        for i in range(j + 1, n):
-            sum2 = 0
-            for l in range(j):
-                sum2 = sum2 + L[i][l] * L[j][l]
-            L[i][j] = (A[i][j] - sum2) / L[j][j]
-
+    L, D = LDL(A)
+    D = np.sqrt(D)
+    L = L @ D
     return L
-
-
-A = np.array([
-    [1.0, 2.0, 3.0, 4.0],
-    [1.0, 4.0, 9.0, 16.0],
-    [1.0, 8.0, 27.0, 64.0],
-    [1.0, 16.0, 81.0, 256.0]
-])
-
-a = np.array([2.0, 10.0, 44.0, 190.0])
-
-B = np.array([
-    [2.0, 1.0, 2.0],
-    [1.0, 2.0, 3.0],
-    [4.0, 1.0, 2.0]
-])
-
-b = np.array([1.0, 0.0, 1.0])
-
-print(cholesky(A))
---
+++ Tests.py
@@ -0,0 +1,215 @@
+from Algoritmos import *
+import traceback
+
+
+'''############# Test Functions #############'''
+
+def Test(func):
+    global tests
+
+    tests.append(func)
+
+    return func
+
+
+def run_tests():
+    exceptions = []
+    for test in tests:
+        try:
+            print("Running {} ... ".format(test.__name__), end='')
+            test()
+            print("OK")
+        except AssertionError as e:
+            error_msg = traceback.format_exc()
+            exceptions.append((test, error_msg))
+            print("ERROR")
+
+    if len(exceptions) > 0:
+        print("\nErrors:\n")
+        for (test, error_msg) in exceptions:
+            print("In {}".format(test.__name__))
+            print(error_msg)
+    else:
+        print("\n\nAll tests passed!")
+
+
+'''############# Tests #############'''
+tests = []
+
+'''##### has_LU Tests #####'''
+
+@Test
+def has_LU_1():
+    A = np.identity(3)
+
+    assert (has_LU(A))
+
+
+@Test
+def has_LU_2():
+    A = np.zeros((3, 3))
+
+    assert (not has_LU(A))
+
+
+@Test
+def has_LU_3():
+    A = np.ones((3, 3))
+
+    assert (not has_LU(A))
+
+
+@Test
+def has_LU_4():
+    A = np.array([
+        [1, 0, 0],
+        [0, 0, 1],
+        [0, 1, 0]
+    ])
+
+    assert (not has_LU(A))
+
+
+'''##### is_orthogonal Tests #####'''
+
+
+@Test
+def is_orthogonal_1():
+    A = np.eye(3)
+
+    assert (is_orthogonal(A))
+
+
+@Test
+def is_orthogonal_2():
+    angle = np.pi / 4
+    A = np.array([
+        [np.cos(angle), -np.sin(angle)],
+        [np.sin(angle), np.cos(angle)]
+    ])
+
+    assert (is_orthogonal(A))
+
+
+@Test
+def is_orthogonal_3():
+    A = np.array([
+        [1, 0],
+        [0, -1]
+    ])
+
+    assert (is_orthogonal(A))
+
+
+'''##### solve Tests #####'''
+
+
+@Test
+def solve_1():
+    A = np.eye(5)
+    b = np.arange(5)
+
+    x = solve(A, b)
+
+    assert (np.allclose(x, b))
+
+
+@Test
+def solve_2():
+    A = np.array([
+        [1.0, 2.0, 3.0, 4.0],
+        [1.0, 4.0, 9.0, 16.0],
+        [1.0, 8.0, 27.0, 64.0],
+        [1.0, 16.0, 81.0, 256.0]
+    ])
+    b = np.array([2.0, 10.0, 44.0, 190.0])
+    x_expected = np.array([-1.0, 1.0, -1.0, 1.0])
+
+    x = solve(A, b)
+
+    assert (np.allclose(x_expected, x))
+
+
+@Test
+def solve_3():
+    A = np.array([
+        [2.0, 1.0, 2.0],
+        [1.0, 2.0, 3.0],
+        [4.0, 1.0, 2.0]
+    ])
+    b = np.array([1.0, 0.0, 1.0])
+    x_expected = np.array([0, -3, 2])
+
+    x = solve(A, b)
+
+    assert (np.allclose(x_expected, x))
+
+
+'''##### LU Tests #####'''
+
+
+@Test
+def LU_1():
+    A = 3 * np.identity(3)
+
+    L, U = LU(A)
+
+    assert (np.allclose(L, np.eye(3)))
+    assert (np.allclose(U, 3 * np.eye(3)))
+
+
+@Test
+def LU_2():
+    L = np.array([
+        [1, 0, 0],
+        [1, 1, 0],
+        [1, 1, 1],
+    ])
+
+    U = np.array([
+        [1, 1, 1],
+        [0, 2, 2],
+        [0, 0, 3],
+    ])
+
+    A = L @ U
+
+    L_obt, U_obt = LU(A)
+
+    assert (np.allclose(L, L_obt))
+    assert (np.allclose(U, U_obt))
+
+
+'''##### cholesky Tests #####'''
+
+
+@Test
+def cholesky_1():
+    A = np.eye(3)
+    L = cholesky(A)
+
+    assert (np.allclose(A, L))
+
+
+@Test
+def cholesky_2():
+    A = 4 * np.eye(3)
+    L = cholesky(A)
+
+    assert (np.allclose(L, 2 * np.eye(3)))
+
+
+@Test
+def cholesky_3():
+    L1 = np.array([
+        [1, 0, 0],
+        [2, 2, 0],
+        [4, 4, 4]
+    ])
+
+    A = L1 @ L1.T
+    L = cholesky(A)
+    assert (np.allclose(L, L1))
+
+
+run_tests()
